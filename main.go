2f5964534ea592efc541a748850af94640a14c1e
Tsuzu <cs3238-tsuzu@users.noreply.github.com>
2020-09-12 17:22:36 +0900

    Disable bot protection

--- main.go
+++ main.go
@@ -265,7 +265,7 @@ func main() {
 	// Middleware
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
-	e.Use(botProtection)
+	// e.Use(botProtection)
 
 	// Initialize
 	e.POST("/initialize", initialize)
